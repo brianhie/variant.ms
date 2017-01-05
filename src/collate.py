b88eab4c43f8985118e797a4d4f93b879e107a32
Brian Hie <blhie1@gmail.com>
2017-01-04 17:06:20 -0800

    Refactored to allow for modular manipulation

--- src/collate.py
+++ src/collate.py
@@ -1,5 +1,3 @@
-import time
-import numpy as np
 
 class Coll(object):
     def __init__(self, sequence):
@@ -12,14 +10,11 @@ class CollElem(object):
 def collate(collate_A, collate_B, similarity):
     A = collate_A.sequence
     B = collate_B.sequence
-
     G = {}
-
     M = len(A)
     N = len(B)
     MAX = max(0, M+N)
     V = {}
-
     V[1] = 0
     x = 0
     y = 0
@@ -36,7 +31,7 @@ def collate(collate_A, collate_B, similarity):
                 j = x - k
             y = x - k
             G[(x, y)] = (i, j)
-            while x < M and y < N and similarity(A[x], B[y]) > 0.5:
+            while x < M and y < N and similarity(A[x], B[y]) > 0.8:
                 i, j = x, y
                 x, y = x+1, y+1
                 G[(x,y)] = (i,j)
@@ -46,6 +41,7 @@ def collate(collate_A, collate_B, similarity):
         if x >= M and y >= N:
             break
 
+    # Trace path back to source.
     (i, j) = (M, N)
     while i > 0 and j > 0:
         pos = G[(i, j)]
@@ -61,52 +57,3 @@ def collate(collate_A, collate_B, similarity):
     while i > 0:
         yield A[i-1], None, 'delete'
         i -= 1
-
-def collate_nw(collate_A, collate_B, similarity):
-    A = collate_A.sequence
-    B = collate_B.sequence
-    F = np.empty((len(A)+1, len(B)+1)) # Scores.
-    G = np.empty((len(A)+1, len(B)+1), dtype='2int32') # Directions.
-    gap_penalty = -1
-    start = time.time()
-    for i in range(len(A)+1):
-        F[i, 0] = gap_penalty * i
-    for j in range(len(B)+1):
-        F[0, j] = gap_penalty * j
-    for i in range(1, len(A)+1):
-        for j in range(1, len(B)+1):
-            match = F[i-1, j-1] + similarity(A[i-1], B[j-1])
-            delete = F[i-1, j] + gap_penalty
-            insert = F[i, j-1] + gap_penalty
-            max_score = max((match, delete, insert))
-            F[i, j] = max_score
-            if max_score == match:
-                G[i, j] = (i-1, j-1)
-            elif max_score == delete:
-                G[i, j] = (i-1, j)
-            elif max_score == insert:
-                G[i, j] = (i, j-1)
-            else:
-                assert(False)
-    end = time.time()
-    print('Matrix construction: ' + str(end-start))
-
-    start = time.time()
-    (i, j) = (len(A), len(B))
-    while i > 0 and j > 0:
-        pos = G[i, j]
-        pos = (pos[0], pos[1])
-        if i > 0 and j > 0 and pos == (i-1, j-1):
-            yield A[i-1], B[j-1], 'match'
-        elif i > 0 and pos == (i-1, j):
-            yield A[i-1], None, 'delete'
-        elif j > 0 and pos == (i, j-1):
-            yield None, B[j-1], 'insert'
-        else:
-            assert(False)
-        (i, j) = pos
-    while i > 0:
-        yield A[i-1], None, 'delete'
-        i -= 1
-    end = time.time()
-    print('Matrix traversal: ' + str(end-start))
--- src/collate_ms.py
+++ src/collate_ms.py
@@ -8,56 +8,153 @@ import os
 import re
 import string
 import sys
+import time
 import uuid
 
+
 class Text(coll.Coll):
-    def __init__(self, text_id):
+    def __init__(self, text_id, corpus_id, content=u'', meta={}, base_text_id=''):
         super(Text, self).__init__([])
         self.text_id = text_id
-        self.title = '(no title)'
-        self.content = ''
-        self.description = ''
+        self.corpus_id = corpus_id
+        self.meta = meta
+        self.tokenize(content=content)
+        self.base_text_id = base_text_id
+
+    def load(self, text_path):
+        content = codecs.open(text_path, encoding='utf8').read()
+        self.tokenize(content=content)
 
     def tokenize(self, content=u''):
-        self.content = content
         if content == '':
             return
+        seq = 0
         for a in re.split(r'(\s+)', content):
             if a.strip() == '':
-                self.sequence.append(Token(a, self.text_id))
+                if self.text_id == '__COLL__':
+                    self.sequence.append(CollToken(a, self.text_id,
+                                                   self.corpus_id, seq,
+                                                   self.base_text_id))
+                else:
+                    self.sequence.append(Token(a, self.text_id,
+                                               self.corpus_id, seq))
+                seq += 1
                 continue
             for b in re.split(ur'([\.?!,:;\-–—―=…\}\]\)\"”’]+$|^[\{\[\(\"“‘]+)',
                               a, re.UNICODE):
                 if b != '':
-                    self.sequence.append(Token(b, self.text_id))
+                    if self.text_id == '__COLL__':
+                        self.sequence.append(CollToken(b, self.text_id,
+                                                       self.corpus_id, seq,
+                                                       self.base_text_id))
+                    else:
+                        self.sequence.append(Token(b, self.text_id,
+                                                   self.corpus_id, seq))
+                    seq += 1
 
+    def ensure_dir(self, dname):
+        if not os.path.exists(dname):
+            os.makedirs(dname)
+
+    def json_dump(self):
+        corpus_dname = '../target/' + self.corpus_id
+        self.ensure_dir(corpus_dname)
+    
+        # Write any meta data about text.
+        text_dname = corpus_dname + '/' + self.text_id
+        self.ensure_dir(text_dname)
+        with open(text_dname + '/meta.json', 'w') as output_json:
+            json.dump(self.meta, output_json)
+    
+        # Write tokens to their respective files.
+        tokens_dname = text_dname + '/tokens'
+        self.ensure_dir(tokens_dname)
+        for token in self.sequence:
+            token.json_dump()
+    
     def add_token(self, token):
         self.sequence.append(token)
 
     def push_token(self, token):
         self.sequence.insert(0, token)
 
-class Token(coll.CollElem):
-    def __init__(self, base_word, base_text_id):
-        self._words = {}
-        self.base_word = base_word
-        self.base_text_id = base_text_id
-        self.add(base_word, base_text_id)
-        super(Token, self).__init__()
-        
-    def add(self, word, text_id):
-        self._words[text_id] = word
 
-    def get(self, text_id):
+class Token(coll.CollElem):
+    def __init__(self, word, text_id, corpus_id, seq, meta={}):
+        self.word = word
+        self.text_id = text_id
+        self.corpus_id = corpus_id
+        self.seq = seq
+        self.meta = meta
+        #self.id = '.'.join([str(f) for f in [ corpus_id, text_id, seq ]])
+
+    def dict_dump(self):
+        coll_token_meta = {}
+        coll_token_meta['word'] = self.word
+        coll_token_meta['text_id'] = self.text_id
+        coll_token_meta['meta'] = self.meta
+        return coll_token_meta
+
+    def json_dump(self):
+        token_meta = self.dict_dump()
+        tokens_dname = ('../target/' + self.corpus_id +
+                        '/' + self.text_id + '/tokens')
+        with open(tokens_dname + '/' +
+                  str(self.seq) + '.json', 'w') as output_json:
+            json.dump(token_meta, output_json)
+
+
+class CollToken(Token):
+    def __init__(self, word, text_id, corpus_id, seq, base_text_id):
+        self.base_text_id = base_text_id
+        self._words = {}
+        self._seqs = {}
+        self.add(word, text_id, seq)
+        super(CollToken, self).__init__(word, text_id, corpus_id, seq)
+
+    def add(self, word, text_id, seqs):
+        self._words[text_id] = word
+        self._seqs[text_id] = seqs
+
+    def add_seq(self, text_id, seq):
+        self._seqs[text_id] += seq
+
+    def get_word(self, text_id):
         return self._words[text_id]
+
+    def get_seq(self, text_id):
+        return self._seqs[text_id]
 
     def iter_words(self):
         for text_id in self._words:
-            yield self._words[text_id]
+            yield self._words[text_id], text_id, self._seqs[text_id]
 
-    def iter_word_texts(self):
-        for text_id in self._words:
-            yield self._words[text_id], text_id
+    def dict_dump(self):
+        coll_token_meta = {}
+        tokens = []
+        for word, text_id, seqs in self.iter_words():
+            tokens.append({
+                'word': word,
+                'text_id': text_id,
+                'seq': seqs
+            })
+            if text_id == self.base_text_id:
+                coll_token_meta['base_word'] = word
+                coll_token_meta['base_text_id'] = text_id
+        coll_token_meta['tokens'] = tokens
+        return coll_token_meta
+
+    def json_dump(self):
+        token_meta = self.dict_dump()
+        tokens_dname = ('../target/' + self.corpus_id +
+                        '/__COLL__/tokens')
+        if not os.path.exists(tokens_dname):
+            os.makedirs(tokens_dname)
+        
+        with open(tokens_dname + '/' +
+                  str(self.seq) + '.json', 'w') as output_json:
+            json.dump(token_meta, output_json)
+
 
 def isspace(s):
     return s.strip() == ''
@@ -68,126 +165,119 @@ def ispunc(s):
 def token_similarity(a, b):
     # Strings are a case insensitive match.
     # Match any whitespace to any whitespace.
-    if a.base_word.lower().strip() == b.base_word.lower().strip():
+    if a.word.lower().strip() == b.word.lower().strip():
         return 1.
 
-    # Make it near impossible for words to map to whitespace.
-    if ((isspace(a.base_word) and not isspace(b.base_word)) or
-        (not isspace(a.base_word) and isspace(b.base_word))):
-        return -100000.
+    # Make it impossible for words to map to whitespace.
+    if ((isspace(a.word) and not isspace(b.word)) or
+        (not isspace(a.word) and isspace(b.word))):
+        return -1.
 
-    # Make it near impossible for words to map to punctuation.
-    if ispunc(a.base_word) and ispunc(b.base_word):
+    # Make it impossible for words to map to punctuation.
+    if ispunc(a.word) and ispunc(b.word):
         return 0.9
-    if ((ispunc(a.base_word) and not ispunc(b.base_word)) or
-        (not ispunc(a.base_word) and ispunc(b.base_word))):
-        return -100000.
+    if ((ispunc(a.word) and not ispunc(b.word)) or
+        (not ispunc(a.word) and ispunc(b.word))):
+        return -1.
 
     # Strings sound alike (approximate phonetic match).
-    if a.base_word.isalpha() and b.base_word.isalpha():
-        if jf.metaphone(a.base_word) == jf.metaphone(b.base_word):
+    if a.word.isalpha() and b.word.isalpha():
+        if jf.metaphone(a.word) == jf.metaphone(b.word):
             return 0.9
-        if jf.soundex(a.base_word) == jf.soundex(b.base_word):
+        if jf.soundex(a.word) == jf.soundex(b.word):
             return 0.9
-        if jf.nysiis(a.base_word) == jf.nysiis(b.base_word):
+        if jf.nysiis(a.word) == jf.nysiis(b.word):
             return 0.9
-        if jf.match_rating_codex(a.base_word) == jf.match_rating_codex(b.base_word):
+        if jf.match_rating_codex(a.word) == jf.match_rating_codex(b.word):
             return 0.9
 
     # Use scaled Jaro-Winkler distance.
-    return (jf.jaro_winkler(a.base_word, b.base_word) * 2) - 1
+    return jf.jaro_winkler(a.word, b.word)
 
-def collate_text(master, text):
-    new_master = Text('__COLL__')
-    to_insert = ''
-    for token_a, token_b, status in coll.collate(master, text,
+def collate_text(coll_text, text):
+    new_coll_text = Text(coll_text.text_id, coll_text.corpus_id)
+    insert_word = ''
+    insert_seq = []
+    for token_a, token_b, status in coll.collate(coll_text, text,
                                                  token_similarity):
         if status == 'match':
-            assert(token_b.base_text_id == text.text_id)
-            token_a.add(token_b.base_word + to_insert, token_b.base_text_id)
-            new_master.push_token(token_a)
-            to_insert = ''
+            assert(token_b.text_id == text.text_id)
+            token_a.add(token_b.word + insert_word, token_b.text_id,
+                        [ token_b.seq ])
+            token_a.add_seq(text.text_id, insert_seq)
+            new_coll_text.push_token(token_a)
+            insert_word = ''
+            insert_seq = []
         elif status == 'delete':
-            token_a.add('' + to_insert, text.text_id)
-            new_master.push_token(token_a)
-            to_insert = ''
+            token_a.add('' + insert_word, text.text_id, [])
+            token_a.add_seq(text.text_id, insert_seq)
+            new_coll_text.push_token(token_a)
+            insert_word = ''
+            insert_seq = []
         elif status == 'insert':
-            to_insert = token_b.base_word + to_insert
+            assert(token_b.text_id == text.text_id)
+            insert_word = token_b.word + insert_word
+            insert_seq.append(token_b.seq)
         else:
             assert(False)
-    if to_insert != '':
-        first_word = new_master.sequence[0].base_word
-        new_master.sequence[0].add(to_insert + first_word, text.text_id)
-    return new_master
-
-def load_text(text_dir, text_path):
-    text_path = text_dir + '/' + text_path
-    text_id = re.sub('\.txt$', '', text_path.split('/')[-1])
-    text_content = codecs.open(text_path, encoding='utf8').read()
-    text = Text(text_id)
-    text.tokenize(text_content)
-    return text
+    if insert_word != '':
+        first_word = new_coll_text.sequence[0].word
+        new_coll_text.sequence[0].add(insert_word + first_word,
+                                      text.text_id)
+        new_coll_text.sequence[0].add_seq(text.text_id, insert_seq)
+    return new_coll_text
 
 def o_coll_print(collated):
     for token in collated.sequence:
-        sys.stdout.write(token.base_word)
+        sys.stdout.write(token.word)
     print('=============')
     for token in collated.sequence:
-        if token.base_word.strip() == '' or len(token._words) == 1:
-            sys.stdout.write(token.base_word)
+        if token.word.strip() == '':
+            sys.stdout.write(token.word)
         else:
             sys.stdout.write('(')
-            sys.stdout.write('|'.join(set([ w for w in token.iter_words() ])))
+            sys.stdout.write('|'.join(set([ w[0] for w in token.iter_words() ])))
             sys.stdout.write(')')
 
-def o_coll_json(collated, name):
-    coll_json = {}
-    coll_json['title'] = 'Collated text'
-    coll_json['text_id'] = str(uuid.uuid1())
-    coll_json['description'] = ''
-    coll_json['tokens'] = []
-    for token in collated.sequence:
-        words = [{
-            'word': token.base_word,
-            'text': token.base_text_id,
-            'is_base': True
-        }]
-        for word, text_id in token.iter_word_texts():
-            if text_id == token.base_text_id and word == token.base_word:
-                continue
-            words.append({
-                'word': word,
-                'text': text_id,
-                'is_base': False
-            })
-        coll_json['tokens'].append(words)
-    
-    output_dname = '../target/' + name
-    if not os.path.exists(output_dname):
-        os.makedirs(output_dname)
-    with open(output_dname + '/collated.json', 'w') as output_json:
-        json.dump(coll_json, output_json, indent=4)
 
 if __name__ == '__main__':
     text_dir = sys.argv[1]
     default_base_fname = 'base.txt'
-    collated = None
-    # If base.txt is provided, use it.
-    import time
+    listdir = os.listdir(text_dir)
+    corpus_id = text_dir.rstrip('/').split('/')[-1]
     start = time.time()
-    if default_base_fname in os.listdir(text_dir):
-        collated = load_text(text_dir, default_base_fname)
-    for text_fname in os.listdir(text_dir):
-        if collated == None:
-            # If base.txt is not provided, use first file in directory.
-            collated = load_text(text_dir, text_fname)
-            continue
-        else:
-            text = load_text(text_dir, text_fname)
-        end = time.time()
-        print('Load text: ' + str(end - start))
-        # Repeatedly collate using a common base text.
-        collated = collate_text(collated, text)
 
-    o_coll_print(collated)
-    o_coll_json(collated, text_dir.rstrip('/').split('/')[-1])
+    # If base.txt is provided, use it.
+    coll_text = None
+    if default_base_fname in listdir:
+        text_id = re.sub('\.txt$', '', default_base_fname.split('/')[-1])
+        coll_text = Text('__COLL__', corpus_id, base_text_id=text_id)
+        coll_text.load(text_dir + '/' + default_base_fname)
+        base_text = Text(text_id, corpus_id)
+        base_text.load(text_dir + '/' + default_base_fname)
+        base_text.json_dump()
+
+    # Repeatedly collate all files in directory.
+    for pos, text_fname in enumerate(listdir):
+        text_id = re.sub('\.txt$', '', text_fname.split('/')[-1])
+        if coll_text == None:
+            # If base.txt is not provided, use first file in directory.
+            coll_text = Text('__COLL__', corpus_id, base_text_id=text_id)
+            coll_text.load(text_dir + '/' + text_fname)
+            base_text = Text(text_id, corpus_id)
+            base_text.load(text_dir + '/' + default_base_fname)
+            base_text.json_dump()
+            continue
+        text = Text(text_id, corpus_id)
+        text.load(text_dir + '/' + text_fname)
+        text.json_dump()
+        # Bulk of collation effort.
+        coll_text = collate_text(coll_text, text)
+        # Performance timer.
+        end = time.time()
+        sys.stdout.write('\r' + str(round(pos/float(len(listdir))*100, 1))
+                         + '%, ' + str(round(end - start, 1)) + 's\033[K')
+    sys.stdout.write('\r100%, ' + str(round(end - start, 1)) + 's\033[K\n')
+
+    o_coll_print(coll_text)
+    coll_text.json_dump()
